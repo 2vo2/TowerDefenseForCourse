bc77ffab449b443c985b1ce15fba04f0081947fa
Yehor <egorzvoznikov@gmail.com>
2025-02-24 22:11:37 +0200

    First try to refactor UI

--- Assets/Scripts/UI/GameInterface.cs
+++ Assets/Scripts/UI/GameInterface.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameInterface : MonoBehaviour
 {
-    
+    [SerializeField] private UIDocument _gameInterface;
+
+    private VisualElement _root;
+
+    private void Awake()
+    {
+        _root = _gameInterface.rootVisualElement;
+
+        var buttons = _root.Query<Button>().Class("unity-button").Build();
+
+        buttons.ForEach(button =>
+        {
+            button.RegisterCallback<ClickEvent>(OnTowerButtonClick);
+        });
+    }
+
+    private void OnTowerButtonClick(ClickEvent evt)
+    {
+        print("Tower");
+    }
 }
