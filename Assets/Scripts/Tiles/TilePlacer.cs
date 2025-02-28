c6f96b6391c95a95e55a826cca4473b5e9b8a324
Yehor <egorzvoznikov@gmail.com>
2025-02-28 19:02:56 +0200

    Refactor TilePlacer
    Generation of a straight road

--- Assets/Scripts/Tiles/TilePlacer.cs
+++ Assets/Scripts/Tiles/TilePlacer.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class TilePlacer : EditorWindow
 {
     public TileToPlace TilePrefab;
+    public Road RoadPrefab;
     public int MinX = -7;
     public int MaxX = 7;
     public int MinZ = -7;
     public int MaxZ = 7;
 
+    private int _roadZ;
+    
     [MenuItem("Tools/Tile Placer")]
     public static void ShowWindow()
     {
@@ -20,6 +23,7 @@ public class TilePlacer : EditorWindow
         GUILayout.Label("Tile Placer Settings", EditorStyles.boldLabel);
 
         TilePrefab = (TileToPlace)EditorGUILayout.ObjectField("Tile Prefab", TilePrefab, typeof(TileToPlace), false);
+        RoadPrefab = (Road)EditorGUILayout.ObjectField("Road Prefab", RoadPrefab, typeof(Road), false);
 
         MinX = EditorGUILayout.IntField("Min X", MinX);
         MaxX = EditorGUILayout.IntField("Max X", MaxX);
@@ -42,14 +46,26 @@ public class TilePlacer : EditorWindow
 
         var parent = new GameObject("TileGrid");
         
+        _roadZ = Random.Range(MinZ, MaxZ + 1);
+        
         for (var x = MinX; x <= MaxX; x++)
         {
             for (var z = MinZ; z <= MaxZ; z++)
             {
                 var position = new Vector3(x, 0, z);
-                var tile = (TileToPlace)PrefabUtility.InstantiatePrefab(TilePrefab);
-                tile.transform.position = position;
-                tile.transform.parent = parent.transform;
+
+                if (z == _roadZ)
+                {
+                    var road = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+                    road.transform.position = position;
+                    road.transform.parent = parent.transform;
+                }
+                else
+                {
+                    var tile = (TileToPlace)PrefabUtility.InstantiatePrefab(TilePrefab);
+                    tile.transform.position = position;
+                    tile.transform.parent = parent.transform;
+                }
             }
         }
 
