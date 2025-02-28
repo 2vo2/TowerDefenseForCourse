cf0974d74ef404e24c73f951555cf0c57d237a71
Yehor <egorzvoznikov@gmail.com>
2025-02-28 17:49:24 +0200

    Add basic object pool to enemy base

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,15 +1,23 @@
-using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Enemy _enemyPrefab;
     [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private int _spawnSize;
     [SerializeField] private float _spawnDelay;
 
+    private List<Enemy> _enemies = new List<Enemy>();
+    
     private void Start()
     {
+        for (var i = 0; i < _spawnSize; i++)
+        {
+            CreateNewEnemy();
+        }
+        
         StartCoroutine(SpawnEnemies());
     }
 
@@ -17,8 +25,38 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)
         {
-            var newEnemy = Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
             yield return new WaitForSeconds(_spawnDelay);
+
+            var enemy = GetInactiveEnemy();
+            
+            if (enemy != null)
+            {
+                enemy.gameObject.SetActive(true);
+            }
+            else
+            {
+                CreateNewEnemy().gameObject.SetActive(true);
+            }
         }
+    }
+
+    private Enemy GetInactiveEnemy()
+    {
+        foreach (var enemy in _enemies)
+        {
+            if (!enemy.gameObject.activeSelf)
+            {
+                return enemy;
+            }
+        }
+        return null;
+    }
+
+    private Enemy CreateNewEnemy()
+    {
+        var newEnemy = Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
+        newEnemy.gameObject.SetActive(false);
+        _enemies.Add(newEnemy);
+        return newEnemy;
     }
 }
