fb088bf104973fc1c37c478205ece1035c2044cb
Yehor <egorzvoznikov@gmail.com>
2025-04-19 16:52:33 +0300

    Add spawn base on grid

--- Assets/Scripts/Tiles/TileGridGenerator.cs
+++ Assets/Scripts/Tiles/TileGridGenerator.cs
@@ -1,13 +1,16 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Serialization;
 
 public class TileGridGenerator : EditorWindow
 {
-    [SerializeField] private TileToPlace TilePrefab;
-    [SerializeField] private Road RoadPrefab;
-    [SerializeField] private int SizeX = 10;
-    [SerializeField] private int SizeZ = 10;
+    [SerializeField] private int _sizeX = 10;
+    [SerializeField] private int _sizeZ = 10;
+    [SerializeField] private TileToPlace _tilePrefab;
+    [SerializeField] private Road _roadPrefab;
+    [SerializeField] private PlayerBase _playerBase;
+    [SerializeField] private EnemySpawner _enemySpawner;
 
     private List<Road> _randomRoads;
 
@@ -19,10 +22,12 @@ public class TileGridGenerator : EditorWindow
 
     private void OnGUI()
     {
-        TilePrefab = (TileToPlace)EditorGUILayout.ObjectField("Tile Prefab", TilePrefab, typeof(TileToPlace), false);
-        RoadPrefab = (Road)EditorGUILayout.ObjectField("Road Prefab", RoadPrefab, typeof(Road), false);
-        SizeX = EditorGUILayout.IntField("Size X", SizeX);
-        SizeZ = EditorGUILayout.IntField("Size Z", SizeZ);
+        _sizeX = EditorGUILayout.IntField("Size X", _sizeX);
+        _sizeZ = EditorGUILayout.IntField("Size Z", _sizeZ);
+        _tilePrefab = (TileToPlace)EditorGUILayout.ObjectField("Tile Prefab", _tilePrefab, typeof(TileToPlace), false);
+        _roadPrefab = (Road)EditorGUILayout.ObjectField("Road Prefab", _roadPrefab, typeof(Road), false);
+        _playerBase = (PlayerBase)EditorGUILayout.ObjectField("Player Base", _playerBase, typeof(PlayerBase), false);
+        _enemySpawner = (EnemySpawner)EditorGUILayout.ObjectField("Enemy Base", _enemySpawner, typeof(EnemySpawner), false);
 
         if (GUILayout.Button("Generate Grid"))
         {
@@ -32,7 +37,7 @@ public class TileGridGenerator : EditorWindow
 
     private void PlaceTiles()
     {
-        if (TilePrefab == null || RoadPrefab == null)
+        if (_tilePrefab == null || _roadPrefab == null)
         {
             Debug.LogError("Assign prefabs first!");
             return;
@@ -41,22 +46,28 @@ public class TileGridGenerator : EditorWindow
         var parent = new GameObject("TileGrid");
         _randomRoads = new List<Road>();
 
-        int halfInnerX = Mathf.CeilToInt((SizeX - 1) / 2f);
-        int innerZ = SizeZ - 1;
+        int halfInnerX = Mathf.CeilToInt((_sizeX - 1) / 2f);
+        int innerZ = _sizeZ - 1;
 
         // Старт
         int startZ = Random.Range(-innerZ, innerZ + 1);
-        var startRoad = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+        var startRoad = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
         startRoad.name = "StartRoad";
-        startRoad.transform.position = new Vector3(-SizeX, 0, startZ);
+        startRoad.transform.position = new Vector3(-_sizeX, 0, startZ);
         startRoad.transform.parent = parent.transform;
 
+        var enemyBase = (EnemySpawner)PrefabUtility.InstantiatePrefab(_enemySpawner);
+        enemyBase.transform.position = new Vector3(-_sizeX - 1, 0, startZ);
+        
         // Фініш
         int finishZ = Random.Range(-innerZ, innerZ + 1);
-        var finishRoad = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+        var finishRoad = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
         finishRoad.name = "FinishRoad";
-        finishRoad.transform.position = new Vector3(SizeX, 0, finishZ);
+        finishRoad.transform.position = new Vector3(_sizeX, 0, finishZ);
         finishRoad.transform.parent = parent.transform;
+        
+        var playerBase = (PlayerBase)PrefabUtility.InstantiatePrefab(_playerBase);
+        playerBase.transform.position = new Vector3(_sizeX + 1, 0, finishZ);
 
         // Випадкові дороги з унікальним Z
         HashSet<int> usedZ = new HashSet<int>();
@@ -64,7 +75,7 @@ public class TileGridGenerator : EditorWindow
         Vector3 leftRandomPos;
         do
         {
-            int randX = Random.Range(-SizeX + 1, -SizeX + 1 + halfInnerX);
+            int randX = Random.Range(-_sizeX + 1, -_sizeX + 1 + halfInnerX);
             int randZ = Random.Range(-innerZ, innerZ + 1);
             leftRandomPos = new Vector3(randX, 0, randZ);
         } while (!usedZ.Add((int)leftRandomPos.z));
@@ -72,18 +83,18 @@ public class TileGridGenerator : EditorWindow
         Vector3 rightRandomPos;
         do
         {
-            int randX = Random.Range(-SizeX + 1 + halfInnerX, SizeX);
+            int randX = Random.Range(-_sizeX + 1 + halfInnerX, _sizeX);
             int randZ = Random.Range(-innerZ, innerZ + 1);
             rightRandomPos = new Vector3(randX, 0, randZ);
         } while (!usedZ.Add((int)rightRandomPos.z));
 
         // Створення випадкових доріг
-        var leftRandom = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+        var leftRandom = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
         leftRandom.name = "LeftRandomRoad";
         leftRandom.transform.position = leftRandomPos;
         leftRandom.transform.parent = parent.transform;
 
-        var rightRandom = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+        var rightRandom = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
         rightRandom.name = "RightRandomRoad";
         rightRandom.transform.position = rightRandomPos;
         rightRandom.transform.parent = parent.transform;
@@ -93,13 +104,13 @@ public class TileGridGenerator : EditorWindow
         ConnectPoints(leftRandomPos, rightRandomPos, parent);
 
         // Спочатку до передфінішу (по X), не на SizeX
-        Vector3 preFinish = new Vector3(SizeX - 1, 0, finishZ);
+        Vector3 preFinish = new Vector3(_sizeX - 1, 0, finishZ);
         ConnectPoints(rightRandomPos, preFinish, parent);
 
         // Тепер від preFinish до Finish (по Z)
         while (Mathf.Abs(preFinish.z - finishZ) > 0.1f)
         {
-            var connection = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+            var connection = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
             connection.name = "FinalConnection_Z";
             connection.transform.position = preFinish;
             connection.transform.parent = parent.transform;
@@ -119,7 +130,7 @@ public class TileGridGenerator : EditorWindow
         {
             current.x += current.x < to.x ? 1 : -1;
 
-            var road = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+            var road = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
             road.name = "Connection_X";
             road.transform.position = current;
             road.transform.parent = parent.transform;
@@ -129,7 +140,7 @@ public class TileGridGenerator : EditorWindow
         {
             current.z += current.z < to.z ? 1 : -1;
 
-            var road = (Road)PrefabUtility.InstantiatePrefab(RoadPrefab);
+            var road = (Road)PrefabUtility.InstantiatePrefab(_roadPrefab);
             road.name = "Connection_Z";
             road.transform.position = current;
             road.transform.parent = parent.transform;
@@ -146,14 +157,14 @@ public class TileGridGenerator : EditorWindow
             occupied.Add(gridPos);
         }
 
-        for (int x = -SizeX; x <= SizeX; x++)
+        for (int x = -_sizeX; x <= _sizeX; x++)
         {
-            for (int z = -SizeZ; z <= SizeZ; z++)
+            for (int z = -_sizeZ; z <= _sizeZ; z++)
             {
                 Vector2Int pos = new Vector2Int(x, z);
                 if (occupied.Contains(pos)) continue;
 
-                var tile = (TileToPlace)PrefabUtility.InstantiatePrefab(TilePrefab);
+                var tile = (TileToPlace)PrefabUtility.InstantiatePrefab(_tilePrefab);
                 tile.name = $"Tile_{x}_{z}";
                 tile.transform.position = new Vector3(x, 0, z);
                 tile.transform.parent = parent.transform;
