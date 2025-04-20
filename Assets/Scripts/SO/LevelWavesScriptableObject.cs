f029e50023bd7275d6107d0d8f31b71b6869b17f
Yehor <egorzvoznikov@gmail.com>
2025-04-20 18:48:26 +0300

    Create SO for level waves

---
+++ Assets/Scripts/SO/LevelWavesScriptableObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SO
+{
+    [Serializable]
+    public class InstanceWaveSettings
+    {
+        public float WaveDuration;
+        public EnemyUnit EnemyType;
+    }
+    
+    [Serializable]
+    public class LevelWaveSettings
+    {
+        public List<InstanceWaveSettings> WaveCount;
+    }
+    
+    [CreateAssetMenu(fileName = "New Level Waves Data", menuName = "Level Waves", order = 0)]
+    public class LevelWavesScriptableObject : ScriptableObject
+    {
+        [SerializeField] private List<LevelWaveSettings> _waves;
+        
+        public List<LevelWaveSettings> Waves => _waves;
+    }
+}
