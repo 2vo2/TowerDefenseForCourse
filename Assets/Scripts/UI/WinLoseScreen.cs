768eefe1d365182f1fa63d8dfda214aeea6fc3fd
Yehor <91755408+2vo2@users.noreply.github.com>
2025-06-09 22:29:35 +0300

    Add different win lose button logic

--- Assets/Scripts/UI/WinLoseScreen.cs
+++ Assets/Scripts/UI/WinLoseScreen.cs
@@ -18,6 +18,18 @@ public class WinLoseScreen : UIScreen
         ShowMenu();
         
         _winningLabel.text = $"YOU {winningText}";
+        
+        if (_winningLabel.text == "YOU WIN!")
+        {
+            _playButton.RegisterCallback<ClickEvent>(evt =>
+            {
+                LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            });
+        }
+        else
+        {
+            _playButton.enabledSelf = false;
+        }
     }
 
     private void LoadScene(int sceneNumber)
@@ -31,9 +43,8 @@ public class WinLoseScreen : UIScreen
         _menuButton = _root.Q<Button>("MenuButton");
         _playButton = _root.Q<Button>("PlayButton");
         _restartButton = _root.Q<Button>("RestartButton");
-        
+
         _menuButton.RegisterCallback<ClickEvent>(evt => LoadScene(0));
-        _playButton.RegisterCallback<ClickEvent>(evt => LoadScene(0));
         _restartButton.RegisterCallback<ClickEvent>(evt => LoadScene(1));
     }
 }
